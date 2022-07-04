4578c17de81582a321baf4966b0a6388251591fc
Fatma Betül SIRIM <fatma36873687@gmail.com>
2022-07-04 13:34:21 +0300

    Initial commit

---
+++ src/main/kotlin/gettingstarted/hellloworl.kt
@@ -0,0 +1,30 @@
+package gettingstarted
+
+import kotlin.math.pow
+
+class hellloworl {
+}
+
+fun main() {
+
+    println("slmm")
+
+    val isteIlkDegiskenim = 3.14.pow(2)
+    println(isteIlkDegiskenim)
+
+
+}
+
+// tekli yorum satırı
+
+/*
+çoklu yorum satırı
+ */
+
+/**
+ * dökümatosyan yorum satrı @param args program dökümanlarını tutar
+ */
+//TODO (Betul) daha sonra bakılcak şey gibi
+
+
+
---
+++ src/main/kotlin/gettingstarted/variables.kt
@@ -0,0 +1,13 @@
+package gettingstarted
+
+class variables {
+}
+
+fun main() {
+    val name: String = "fatma betul"
+    var surName: String = "SIRIM"
+
+    println(name)
+
+}
+// val sabit var olursa değiştirebilirsin
